f444ca4e54bc836343d910873f120d4448763446
Simon de Haan <simon@praekeltfoundation.org>
2012-06-25 15:12:07 +0200

    better test setup

--- tests/test_hrb.py
+++ tests/test_hrb.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from twisted.trial.unittest import TestCase
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks
@@ -12,21 +13,30 @@ class HrbTestCase(TestCase):
     timeout = 1
 
     def setUp(self):
-        self.site_factory = Site(BounceResource([self.add_header]))
-        self.port = reactor.listenTCP(0, self.site_factory)
-        addr = self.port.getHost()
-        self.url = "http://%s:%s/" % (addr.host, addr.port)
+        self.default_handlers = [
+            lambda request: request.setHeader('X-UA-Type', 'small'),
+            lambda request: request.setHeader('X-UA-Category', 'mobi'),
+        ]
+        self._running_handlers = []
 
-    def add_header(self, request):
-        request.setHeader('X-UA-Type', 'small')
-        return ''
+    def start_handlers(self, handlers):
+        site_factory = Site(BounceResource(handlers))
+        port = reactor.listenTCP(0, site_factory)
+        addr = port.getHost()
+        url = "http://%s:%s/" % (addr.host, addr.port)
+        self._running_handlers.append(port)
+        return url
 
     def tearDown(self):
-        self.port.loseConnection()
+        for port in self._running_handlers:
+            port.loseConnection()
 
     @inlineCallbacks
     def test_response(self):
-        response = yield http.request(self.url)
+        url = self.start_handlers(self.default_handlers)
+        response = yield http.request(url)
         self.assertEqual(response.delivered_body, '')
         self.assertEqual(response.headers.getRawHeaders('X-UA-Type'),
                             ['small'])
+        self.assertEqual(response.headers.getRawHeaders('X-UA-Category'),
+                            ['mobi'])

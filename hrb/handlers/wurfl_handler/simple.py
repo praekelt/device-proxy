2e890104f22fc9345085e7ac2e2333a43f335af3
Simon de Haan <simon@praekeltfoundation.org>
2012-07-10 13:12:33 +0300

    Change resolution check to <= instead of <

--- hrb/handlers/wurfl_handler/simple.py
+++ hrb/handlers/wurfl_handler/simple.py
@@ -4,7 +4,7 @@ from hrb.handlers.wurfl_handler.base import WurflHandler
 class SimpleWurflHandler(WurflHandler):
 
     def handle_device(self, request, device):
-        if device.resolution_width < 240:
+        if device.resolution_width <= 240:
             request.addCookie(self.cookie_name, 'medium')
         else:
             request.addCookie(self.cookie_name, 'high')

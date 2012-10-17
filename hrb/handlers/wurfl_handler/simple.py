a97264159ddf01feceb44edd00daf65b82c2d12c
Simon de Haan <simon@praekeltfoundation.org>
2012-10-17 15:45:47 +0200

    reworking this thing to be a reverse proxy w/ header injection instead

--- hrb/handlers/wurfl_handler/simple.py
+++ hrb/handlers/wurfl_handler/simple.py
@@ -1,14 +1,10 @@
-from twisted.web import http
-
 from hrb.handlers.wurfl_handler.base import WurflHandler
 
 
 class SimpleWurflHandler(WurflHandler):
 
     def handle_device(self, request, device):
-        if device.resolution_width <= 240:
-            request.addCookie(self.cookie_name, 'medium')
+        if device.resolution_width < 240:
+            return {self.header_name: 'medium'}
         else:
-            request.addCookie(self.cookie_name, 'high')
-        request.code = http.FOUND
-        request.setHeader('Location', request.uri)
+            return {self.header_name: 'high'}
---
+++ tests/test_proxy.py
@@ -0,0 +1,27 @@
+from twisted.internet import defer
+from twisted.internet.defer import inlineCallbacks
+
+from hrb.utils import http
+from tests.utils import TestHandler, HRBTestCase
+
+
+class ProxyTestCase(HRBTestCase):
+
+    @inlineCallbacks
+    def setUp(self):
+        yield super(ProxyTestCase, self).setUp()
+        self.header_handlers = yield self.start_handlers(map(TestHandler, [
+            lambda request: defer.succeed({'X-UA-Type': 'small'}),
+            lambda request: defer.succeed({'X-UA-Category': 'mobi'}),
+            ]))
+
+    @inlineCallbacks
+    def test_setting_header(self):
+        proxy, url = self.start_proxy(self.header_handlers)
+        resp = yield http.request(url, method='GET')
+        self.assertEqual(resp.delivered_body, 'foo')
+        req = yield self.mocked_backend.queue.get()
+        self.assertEqual(req.requestHeaders.getRawHeaders('x-ua-type'),
+            ['small'])
+        self.assertEqual(req.requestHeaders.getRawHeaders('x-ua-category'),
+            ['mobi'])

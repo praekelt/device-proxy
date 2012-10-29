ca5d33c397ae2b0befca83897bef039d14efd77b
Milton <milton@praekeltfoundation.org>
2012-10-29 16:36:58 +0200

    changed min width to <=240

--- devproxy/handlers/wurfl_handler/simple.py
+++ devproxy/handlers/wurfl_handler/simple.py
@@ -4,7 +4,7 @@ from devproxy.handlers.wurfl_handler.base import WurflHandler
 class SimpleWurflHandler(WurflHandler):
 
     def handle_device(self, request, device):
-        if device.resolution_width < 240:
+        if device.resolution_width <= 240:
             return [{self.header_name: 'medium'}]
         else:
             return [{self.header_name: 'high'}]

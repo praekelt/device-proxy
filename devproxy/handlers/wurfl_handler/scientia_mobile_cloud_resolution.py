dc699ef0476c56a5b6f37c3c78701a6b4c56e5b9
Peter Pistorius <peter.pistorius@gmail.com>
2013-01-23 13:01:17 +0200

    class rename.

--- devproxy/handlers/wurfl_handler/scientia_mobile_cloud_resolution.py
+++ devproxy/handlers/wurfl_handler/scientia_mobile_cloud_resolution.py
@@ -5,6 +5,8 @@ class ScientiaMobileCloudResolutionHandler(ScientiaMobileCloudHandler):
 
     def handle_device(self, request, device):
         # check resolution:
+        print device['capabilities']
+        
         if device['id'] == 'apple_iphone_ver2_2_1':
             return [{self.header_name: 'high'}]
         else:

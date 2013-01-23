87c7cda9a3f706968c0733a94237dc53551718cd
Peter Pistorius <peter.pistorius@gmail.com>
2013-01-23 13:03:16 +0200

    more meaningful implementation for scientia mobile cloud subclass.

--- devproxy/handlers/wurfl_handler/scientia_mobile_cloud_resolution.py
+++ devproxy/handlers/wurfl_handler/scientia_mobile_cloud_resolution.py
@@ -4,10 +4,7 @@ from devproxy.handlers.wurfl_handler.scientia_mobile_cloud import ScientiaMobile
 class ScientiaMobileCloudResolutionHandler(ScientiaMobileCloudHandler):
 
     def handle_device(self, request, device):
-        # check resolution:
-        print device['capabilities']
-        
-        if device['id'] == 'apple_iphone_ver2_2_1':
+        if device['capabilities']['resolution_width'] > 240:
             return [{self.header_name: 'high'}]
         else:
             return [{self.header_name: 'medium'}]
